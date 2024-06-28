55cb5c6dd458adff349fba6fa4ff4678ec18d514
Ailurusddog <160128615+Ailurusddog@users.noreply.github.com>
2024-06-29 00:49:36 +0100

    Add code comments

--- app.py
+++ app.py
@@ -3,47 +3,55 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, SubmitField, SelectField, RadioField
 from wtforms.validators import DataRequired
 
+# Initialize the Flask application
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['SECRET_KEY'] = 'your_secret_key'  # Set a secret key for CSRF protection
 
+# Define the questionnaire form class using WTForms
 class QuestionnaireForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired()])
-    student_number = StringField('Student Number', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired()])
-    course = StringField('Programme of Study', validators=[DataRequired()])
-    grades = StringField('Grades Obtained', validators=[DataRequired()])
-    short_term_goal = StringField('Short Term Goal', validators=[DataRequired()])
-    long_term_goal = TextAreaField('Long Term Goal', validators=[DataRequired()])
-    satisfaction = SelectField('Overall Satisfaction', choices=[
+    # Define form fields with labels and validators
+    name = StringField('Name', validators=[DataRequired()])  # Name field
+    student_number = StringField('Student Number', validators=[DataRequired()])  # Student number field
+    email = StringField('Email', validators=[DataRequired()])  # Email field
+    course = StringField('Programme of Study', validators=[DataRequired()])  # Course field
+    grades = StringField('Grades Obtained', validators=[DataRequired()])  # Grades field
+    short_term_goal = StringField('Short Term Goal', validators=[DataRequired()])  # Short term goal field
+    long_term_goal = TextAreaField('Long Term Goal', validators=[DataRequired()])  # Long term goal field
+    satisfaction = SelectField('Overall Satisfaction', choices=[  # Satisfaction select field
         ('very_satisfied', 'Very Satisfied'),
         ('satisfied', 'Satisfied'),
         ('neutral', 'Neutral'),
         ('dissatisfied', 'Dissatisfied'),
         ('very_dissatisfied', 'Very Dissatisfied')
     ], validators=[DataRequired()])
-    improvement = RadioField('Suggestions for Improvement', choices=[
+    improvement = RadioField('Suggestions for Improvement', choices=[  # Improvement suggestions radio field
         ('more_practice', 'More Practice'),
         ('better_materials', 'Better Materials'),
         ('more_support', 'More Support')
     ], validators=[DataRequired()])
-    submit = SubmitField('Submit')
+    submit = SubmitField('Submit')  # Submit button
 
-@app.route('/', methods=['GET'])
+# Define route for the home page
+@app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html')  # Render the home.html template
 
+# Define route for the information page
 @app.route('/information')
 def information():
-    return render_template('information.html')
+    return render_template('information.html')  # Render the information.html template
 
+# Define route for the data collection page
 @app.route('/data', methods=['GET', 'POST'])
 def data():
-    form = QuestionnaireForm()
-    if form.validate_on_submit():
+    form = QuestionnaireForm()  # Create an instance of the questionnaire form
+    if form.validate_on_submit():  # Check if form submission is valid
+        # Open the data.txt file in append mode and write form data to it
         with open('data.txt', 'a') as f:
             f.write(f"{form.name.data}, {form.student_number.data}, {form.email.data}, {form.course.data}, {form.grades.data}, {form.short_term_goal.data}, {form.long_term_goal.data}, {form.satisfaction.data}, {form.improvement.data}\n")
-        return 'Form submitted!'
-    return render_template('data.html', form=form)
+        return 'Form submitted!'  # Display a confirmation message
+    return render_template('data.html', form=form)  # Render the data.html template and pass the form instance
 
+# Run the Flask application in debug mode
 if __name__ == '__main__':
     app.run(debug=True)
